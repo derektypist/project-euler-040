3155630fc8b58f865962ff633d70d1f1c2ffa2e1
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-09-21 10:24:46 +0000

    Added clearInfo function at script.js

--- js/script.js
+++ js/script.js
@@ -37,3 +37,9 @@ function champernownesConstant(n) {
     }
     return product;
 }
+
+// Function to Clear Information
+function clearInfo() {
+    let txt = "";
+    document.getElementById("numinfo").innerHTML = txt;
+}
